512bb6af8e7c9698b0e1393a171755d65c930963
mocmeo <ngnlam96@hotmail.com>
2019-05-28 11:27:04 +0700

    Initialize the extension repository

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    configureWebpack: {
+        plugins: [
+            new CopyPlugin([{
+                    from: path.resolve(__dirname, "./template/manifest.template.json"),
+                    to: path.resolve(__dirname, "./dist/manifest.json")
+                },
+                {
+                    from: path.resolve(__dirname, "./template/images"),
+                    to: path.resolve(__dirname, "./dist/images")
+                }
+            ])
+        ]
+    }
+};
